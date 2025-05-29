a6f742a09c9c10a5c43bd6c623e3db9c137162a1
Achant21 <achant@miumg.edu.gt>
2025-05-28 20:10:57 -0600

    Cambios Anthony

--- src/layout/Sidebar.js
+++ src/layout/Sidebar.js
@@ -1,4 +1,4 @@
-import React, { useState } from 'react';
+import React, { useState, useEffect } from 'react';
 import { Link } from 'react-router-dom';
 import 'boxicons/css/boxicons.min.css';
 import "../style/sidebar.css";
@@ -8,9 +8,26 @@ const Sidebar = () => {
     const [showRegistros, setShowRegistros] = useState(false);
     const [showMenu, setShowMenu] = useState(false);
     const [showCocina, setShowCocina] = useState(false);
-    const [showMesero, setShowMesero] =useState(false);
-    const [showClientes, setShowClientes] =useState(false);
-    const [showReportes, setShowReportes] =useState(false);
+    const [showMesero, setShowMesero] = useState(false);
+    const [showClientes, setShowClientes] = useState(false);
+    const [showReportes, setShowReportes] = useState(false);
+    const [showDropDown, setShowDropDown] = useState(true);
+
+    const [usuario, setUsuario] = useState(null);
+
+    useEffect(() => {
+        const usuarioString = localStorage.getItem("user");
+
+        if (usuarioString) {
+            try {
+                const usuarioObj = JSON.parse(usuarioString); // convierte a objeto
+                setUsuario(usuarioObj);
+            } catch (error) {
+                console.error("JSON inválido en localStorage", error);
+            }
+        }
+    }, []);
+
 
     const toggleSidebar = () => {
         setIsOpen(!isOpen);
@@ -46,7 +63,7 @@ const Sidebar = () => {
 
     const toggleMesero = () => {
         setShowMesero(!showMesero);
-        setShowRegistros(false);
+        setShowRegistros(showMesero);
         setShowMenu(false);
     };
 
@@ -71,9 +88,15 @@ const Sidebar = () => {
                 <div className="sidebar-overlay d-md-none" onClick={closeSidebar}></div>
             )}
 
-            <div
+            {/* <div
                 className={`sidebar-container ${isOpen ? 'open' : ''} vh-100 p-3 d-flex flex-column d-md-block`}
-            >
+            > */}
+
+
+            <div className={`sidebar-container ${isOpen ? 'open' : ''} min-vh-100 p-3 d-flex flex-column d-md-block`}>
+
+
+
                 <h4 className="text-white">MiraLago</h4>
                 <ul className="nav flex-column">
                     <li className="nav-item mb-2">
@@ -82,10 +105,9 @@ const Sidebar = () => {
                             <span>Home</span>
                         </Link>
                     </li>
-                    
 
-                   {/* Modulo Usuarios*/}
-                   <li className="nav-item mb-2">
+                    {/* Modulo Usuarios*/}
+                    {usuario?.rol_id !== 2 && (<li className="nav-item mb-2">
                         <button
                             className="btn btn-link nav-link text-white d-flex align-items-center"
                             onClick={toggleRegistros}
@@ -123,10 +145,13 @@ const Sidebar = () => {
                                 </li>
                             </ul>
                         )}
-                    </li>
+                    </li>)}
+
+
 
                     {/* Modulo Clientes*/}
-                    <li className="nav-item mb-2">
+
+                    {usuario?.rol_id !== 2 && (<li className="nav-item mb-2">
                         <button
                             className="btn btn-link nav-link text-white d-flex align-items-center"
                             onClick={toggleClientes}
@@ -152,60 +177,65 @@ const Sidebar = () => {
                                 </li>
                             </ul>
                         )}
-                    </li>
+                    </li>)}
+
 
                     {/* Modulo Menu */}
-                    <li className="nav-item mb-2">
-                        <button
-                            className="btn btn-link nav-link text-white d-flex align-items-center"
-                            onClick={toggleMenu}
-                        >
-                            <i className='bx bxs-food-menu'></i>
-                            <span className="me-3">Menu</span>
-                            <i className={`bi ms-auto ${showMenu ? 'bi-chevron-up' : 'bi-chevron-down'}`}></i>
-                        </button>
 
-                        {showMenu && (
-                            <ul className="nav flex-column ms-3">
-                                <li className="nav-item mb-2">
-                                    <Link to="/categoriaplato" className="nav-link text-white d-flex align-items-center" onClick={closeSidebar}>
-                                        <i className="bi bi-menu-button-fill me-2"></i>
-                                        <span>Categoria Platillo</span>
-                                    </Link>
-                                </li>
-                                <li className="nav-item mb-2">
-                                    <Link to="/plato" className="nav-link text-white d-flex align-items-center" onClick={closeSidebar}>
-                                        <i className="bi bi-egg-fried me-2"></i>
-                                        <span>Platillo</span>
-                                    </Link>
-                                </li>
-                                <li className="nav-item mb-2">
-                                    <Link to="/mesa" className="nav-link text-white d-flex align-items-center" onClick={closeSidebar}>
-                                        <i className="bi bi-align-top me-2"></i>
-                                        <span>Mesa</span>
-                                    </Link>
-                                </li>
-                                {/* <li className="nav-item mb-2">
-                                    <Link to="/orden" className="nav-link text-white d-flex align-items-center">
-                                        <i className="bi bi-receipt me-2"></i>
-                                        <span>Orden</span>
-                                    </Link>
-                                </li>
-                                <li className="nav-item mb-2">
-                                    <Link to="/detalleorden" className="nav-link text-white d-flex align-items-center">
-                                        <i className="bi bi-receipt-cutoff me-2"></i>
-                                        <span>Detalle Orden</span>
-                                    </Link>
-                                </li> */}
-                                <li className="nav-item mb-2">
-                                    <Link to="/reporteplato" className="nav-link text-white d-flex align-items-center" onClick={closeSidebar}>
-                                        <i className="bi bi-card-heading me-2"></i>
-                                        <span>Reporte Platillo</span>
-                                    </Link>
-                                </li>
-                            </ul>
-                        )}
-                    </li>
+                    {usuario?.rol_id !== 2 && (
+                        <li className="nav-item mb-2">
+                            <button
+                                className="btn btn-link nav-link text-white d-flex align-items-center"
+                                onClick={toggleMenu}
+                            >
+                                <i className='bx bxs-food-menu'></i>
+                                <span className="me-3">Menu</span>
+                                <i className={`bi ms-auto ${showMenu ? 'bi-chevron-up' : 'bi-chevron-down'}`}></i>
+                            </button>
+
+                            {showMenu && (
+                                <ul className="nav flex-column ms-3">
+                                    <li className="nav-item mb-2">
+                                        <Link to="/categoriaplato" className="nav-link text-white d-flex align-items-center" onClick={closeSidebar}>
+                                            <i className="bi bi-menu-button-fill me-2"></i>
+                                            <span>Categoria Platillo</span>
+                                        </Link>
+                                    </li>
+                                    <li className="nav-item mb-2">
+                                        <Link to="/plato" className="nav-link text-white d-flex align-items-center" onClick={closeSidebar}>
+                                            <i className="bi bi-egg-fried me-2"></i>
+                                            <span>Platillo</span>
+                                        </Link>
+                                    </li>
+                                    <li className="nav-item mb-2">
+                                        <Link to="/mesa" className="nav-link text-white d-flex align-items-center" onClick={closeSidebar}>
+                                            <i className="bi bi-align-top me-2"></i>
+                                            <span>Mesa</span>
+                                        </Link>
+                                    </li>
+                                    {/* <li className="nav-item mb-2">
+                                <Link to="/orden" className="nav-link text-white d-flex align-items-center">
+                                    <i className="bi bi-receipt me-2"></i>
+                                    <span>Orden</span>
+                                </Link>
+                            </li>
+                            <li className="nav-item mb-2">
+                                <Link to="/detalleorden" className="nav-link text-white d-flex align-items-center">
+                                    <i className="bi bi-receipt-cutoff me-2"></i>
+                                    <span>Detalle Orden</span>
+                                </Link>
+                            </li> */}
+                                    <li className="nav-item mb-2">
+                                        <Link to="/reporteplato" className="nav-link text-white d-flex align-items-center" onClick={closeSidebar}>
+                                            <i className="bi bi-card-heading me-2"></i>
+                                            <span>Reporte Platillo</span>
+                                        </Link>
+                                    </li>
+                                </ul>
+                            )}
+                        </li>
+                    )}
+
                     {/* Modulo Mesero */}
                     <li className="nav-item mb-2">
                         <button
@@ -239,72 +269,80 @@ const Sidebar = () => {
                                 </li>
                             </ul>
                         )}
-                        </li>
-                    {/* Modulo Cocina */}
-                    <li className="nav-item mb-2">
-                        <button
-                            className="btn btn-link nav-link text-white d-flex align-items-center"
-                            onClick={toggleCocina}
-                        >
-                            <i className="bi bi-basket me-2"></i>
-                            <span className="me-3">Cocina</span>
-                            <i className={`bi ms-auto ${showCocina ? 'bi-chevron-up' : 'bi-chevron-down'}`}></i>
-                        </button>
-
-                        {showCocina && (
-                            <ul className="nav flex-column ms-3">
-                                <li className="nav-item mb-2">
-                                    <Link to="/Cocina" className="nav-link text-white d-flex align-items-center" onClick={closeSidebar}>
-                                        <i className="bi bi-hourglass-split me-2"></i>
-                                        <span>Órdenes Activas</span>
-                                    </Link>
-                                </li>
-                            </ul>
-                        )}
                     </li>
-                    {/* Modulo Reportes */}
-                    <li className="nav-item mb-2">
-                        <button
-                            className="btn btn-link nav-link text-white d-flex align-items-center"
-                            onClick={toggleReportes}
-                        >
-                        <i className='bx bxs-report'></i>
-                            <span className="me-3">Reportes</span>
-                            <i className={`bi ms-auto ${showReportes ? 'bi-chevron-up' : 'bi-chevron-down'}`}></i>
-                        </button>
+                    {/* Modulo Cocina */}
 
-                        {showReportes && (
-                            <ul className="nav flex-column ms-3">
-                                <li className="nav-item mb-2">
-                                    <Link to="/ventasdiarias" className="nav-link text-white d-flex align-items-center" onClick={closeSidebar}>
-                                        <i className='bi bi-file-earmark-bar-graph-fill'></i>
-                                        <span>Ventas Diarias</span>
-                                    </Link>
-                                </li>
-                                <li className="nav-item mb-2">
-                                    <Link to="/repventasplatillo " className="nav-link text-white d-flex align-items-center" onClick={closeSidebar}>
-                                        <i className='bi bi-file-earmark-bar-graph-fill'></i>
-                                        <span>Ventas por Platillos</span>
-                                    </Link>
-                                </li>
-                                <li className="nav-item mb-2">
-                                    <Link to="/repventasmesa" className="nav-link text-white d-flex align-items-center" onClick={closeSidebar}> 
-                                        <i className='bi bi-file-earmark-bar-graph-fill'></i>
-                                        <span>Ventas por Mesa</span>
-                                    </Link>
-                                </li>
-                                <li className="nav-item mb-2">
-                                    <Link to="/repventasmes" className="nav-link text-white d-flex align-items-center" onClick={closeSidebar}>
-                                        <i className='bi bi-file-earmark-bar-graph-fill'></i>
-                                        <span>Ventas por Mes</span>
-                                    </Link>
-                                </li>
-                            </ul>
-                        )}
+                    {usuario?.rol_id !== 2 && (
+                        <li className="nav-item mb-2">
+                            <button
+                                className="btn btn-link nav-link text-white d-flex align-items-center"
+                                onClick={toggleCocina}
+                            >
+                                <i className="bi bi-basket me-2"></i>
+                                <span className="me-3">Cocina</span>
+                                <i className={`bi ms-auto ${showCocina ? 'bi-chevron-up' : 'bi-chevron-down'}`}></i>
+                            </button>
+
+                            {showCocina && (
+                                <ul className="nav flex-column ms-3">
+                                    <li className="nav-item mb-2">
+                                        <Link to="/Cocina" className="nav-link text-white d-flex align-items-center" onClick={closeSidebar}>
+                                            <i className="bi bi-hourglass-split me-2"></i>
+                                            <span>Órdenes Activas</span>
+                                        </Link>
+                                    </li>
+                                </ul>
+                            )}
                         </li>
+                    )}
+
+                    {/* Modulo Reportes */}
+
+                    {usuario?.rol_id !== 2 && (
+                        <li className="nav-item mb-2">
+                            <button
+                                className="btn btn-link nav-link text-white d-flex align-items-center"
+                                onClick={toggleReportes}
+                            >
+                                <i className='bx bxs-report'></i>
+                                <span className="me-3">Reportes</span>
+                                <i className={`bi ms-auto ${showReportes ? 'bi-chevron-up' : 'bi-chevron-down'}`}></i>
+                            </button>
+
+                            {showReportes && (
+                                <ul className="nav flex-column ms-3">
+                                    <li className="nav-item mb-2">
+                                        <Link to="/ventasdiarias" className="nav-link text-white d-flex align-items-center" onClick={closeSidebar}>
+                                            <i className='bi bi-file-earmark-bar-graph-fill'></i>
+                                            <span>Ventas Diarias</span>
+                                        </Link>
+                                    </li>
+                                    <li className="nav-item mb-2">
+                                        <Link to="/repventasplatillo " className="nav-link text-white d-flex align-items-center" onClick={closeSidebar}>
+                                            <i className='bi bi-file-earmark-bar-graph-fill'></i>
+                                            <span>Ventas por Platillos</span>
+                                        </Link>
+                                    </li>
+                                    <li className="nav-item mb-2">
+                                        <Link to="/repventasmesa" className="nav-link text-white d-flex align-items-center" onClick={closeSidebar}>
+                                            <i className='bi bi-file-earmark-bar-graph-fill'></i>
+                                            <span>Ventas por Mesa</span>
+                                        </Link>
+                                    </li>
+                                    <li className="nav-item mb-2">
+                                        <Link to="/repventasmes" className="nav-link text-white d-flex align-items-center" onClick={closeSidebar}>
+                                            <i className='bi bi-file-earmark-bar-graph-fill'></i>
+                                            <span>Ventas por Mes</span>
+                                        </Link>
+                                    </li>
+                                </ul>
+                            )}
+                        </li>
+                    )}
+
                     <li className="nav-item mb-2">
                         <Link to="/caja" className="nav-link text-white d-flex align-items-center" onClick={closeSidebar}>
-                        <i className='bx bx-money-withdraw'></i>
+                            <i className='bx bx-money-withdraw'></i>
                             <span>Cuadre de Caja</span>
                         </Link>
                     </li>
